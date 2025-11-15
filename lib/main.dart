4010c5904360b5714efaa63d214cd05c25da188e
Shangar KI <ki.shangarbackup@gmail.com>
2025-11-15 20:32:03 +0530

    Removed PipView and added native PiP code

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,15 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_inappwebview/flutter_inappwebview.dart';
-import 'package:pip_view/pip_view.dart';
 import 'package:wakelock_plus/wakelock_plus.dart';
+import 'package:flutter/services.dart';
 
 void main() {
   WidgetsFlutterBinding.ensureInitialized();
-  runApp(const YouTubeBrowserApp());
+  runApp(const MyApp());
 }
 
-class YouTubeBrowserApp extends StatelessWidget {
-  const YouTubeBrowserApp({super.key});
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
@@ -30,14 +30,12 @@ class YouTubeBrowser extends StatefulWidget {
 }
 
 class _YouTubeBrowserState extends State<YouTubeBrowser> {
-  InAppWebViewController? webViewController;
-
-  final url = WebUri("https://m.youtube.com");
+  InAppWebViewController? controller;
 
   @override
   void initState() {
     super.initState();
-    WakelockPlus.enable(); // Keep screen awake
+    WakelockPlus.enable();
   }
 
   @override
@@ -46,59 +44,48 @@ class _YouTubeBrowserState extends State<YouTubeBrowser> {
     super.dispose();
   }
 
+  void enterPipMode() {
+    const platform = MethodChannel('pip_channel');
+    platform.invokeMethod("enterPip");
+  }
+
   @override
   Widget build(BuildContext context) {
-    return PipView(
-      builder: (context, isFloating) {
-        return Scaffold(
-          body: SafeArea(
-            child: Column(
-              children: [
-                Expanded(
-                  child: InAppWebView(
-                    initialUrlRequest: URLRequest(url: url),
-                    initialSettings: InAppWebViewSettings(
-                      javaScriptEnabled: true,
-                      allowsInlineMediaPlayback: true,
-                      mediaPlaybackRequiresUserGesture: false,
-                      allowsPictureInPictureMediaPlayback: true,
-                      disallowOverScroll: true,
-                    ),
-                    onWebViewCreated: (controller) {
-                      webViewController = controller;
-                    },
-                    shouldOverrideUrlLoading: (controller, action) async {
-                      final String newUrl = action.request.url.toString();
-
-                      // Allow ONLY YouTube
-                      if (newUrl.contains("youtube.com")) {
-                        return NavigationActionPolicy.ALLOW;
-                      }
-                      return NavigationActionPolicy.CANCEL;
-                    },
-                  ),
+    return Scaffold(
+      body: SafeArea(
+        child: Column(
+          children: [
+            Expanded(
+              child: InAppWebView(
+                initialUrlRequest:
+                    URLRequest(url: WebUri("https://m.youtube.com")),
+                initialSettings: InAppWebViewSettings(
+                  javaScriptEnabled: true,
+                  mediaPlaybackRequiresUserGesture: false,
+                  allowsInlineMediaPlayback: true,
+                  allowsPictureInPictureMediaPlayback: true,
                 ),
-
-                // 📌 PiP BUTTON
-                Container(
-                  margin: const EdgeInsets.only(bottom: 12),
-                  child: ElevatedButton.icon(
-                    style: ElevatedButton.styleFrom(
-                      backgroundColor: Colors.redAccent,
-                      foregroundColor: Colors.white,
-                    ),
-                    onPressed: () {
-                      PipView.of(context)!.present();
-                    },
-                    icon: const Icon(Icons.picture_in_picture),
-                    label: const Text("Enable PiP"),
-                  ),
-                )
-              ],
+                onWebViewCreated: (c) => controller = c,
+                shouldOverrideUrlLoading: (controller, action) async {
+                  final url = action.request.url.toString();
+                  if (url.contains("youtube.com")) {
+                    return NavigationActionPolicy.ALLOW;
+                  }
+                  return NavigationActionPolicy.CANCEL;
+                },
+              ),
             ),
-          ),
-        );
-      },
+            Padding(
+              padding: const EdgeInsets.all(12),
+              child: ElevatedButton.icon(
+                onPressed: enterPipMode,
+                icon: const Icon(Icons.picture_in_picture),
+                label: const Text("Enable PiP"),
+              ),
+            )
+          ],
+        ),
+      ),
     );
   }
 }
